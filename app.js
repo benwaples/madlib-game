3e63f5c50f5a9bf111610d80cbbbbe4f3741de45
Heather Peterson <hpeterson462@gmail.com>
2020-06-26 16:23:46 -0700

    input and button on js working

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // import functions and grab DOM elements
-
+const input1 = document.getElementById('ingWord-1');
+//console.log(input1);
 // initialize state
-
+const result = document.getElementById('results');
 // set event listeners to update state and DOM 
+
+const button = document.getElementById('submitButton');
+//console.log(button);
+
+button.addEventListener('click', () => {
+  //console.log('yes');
+  const userInput = input1.value;
+  result.textContent = userInput;
+  //console.log(userInput);
+});
