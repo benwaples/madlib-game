6c381eadfc3b362e659b1b6faa7ebedcacc1cbc6
Annalise Murphy <nali247@gmail.com>
2020-06-26 17:51:52 -0700

    added extra input and reprogrammed hidden feature/ currenly broken - needs work

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // import functions and grab DOM elements
 const input1 = document.getElementById('ingWord-1');
+const input2 = document.getElementById('ingWord-2');
 //console.log(input1);
 // initialize state
-const result = document.getElementById('results');
+const result1 = document.getElementById('results-1');
+const result2 = document.getElementsById('results-2');
 // set event listeners to update state and DOM 
 
 const button = document.getElementById('submitButton');
 //console.log(button);
 
+const madLib = document.getElementsByClassName('mad-lib');
+
 button.addEventListener('click', () => {
   //console.log('yes');
-  const userInput = input1.value;
-  result.textContent = userInput;
-  result.classList.remove('hidden');
+
+    const userInput1 = input1.value;
+    const userInput2 = input2.value;
+
+    result1.textContent = userInput1;
+    result2.textContent = userInput2;
+    madLib.classlist.remove('hidden');
+
   //console.log(userInput);
+
 });
